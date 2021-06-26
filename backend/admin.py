22104084129d003ea904218b71fc9008ee6b3f20
danielsumah <danielsumah@gmail.com>
2021-06-26 14:44:58 +0100

    created  GeneralRegistration model

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import EmployerRegistration, StudentRegistration
+from .models import EmployerRegistration, StudentRegistration, GeneralRegistration
 
 
 # Register your models here.
 admin.site.register(EmployerRegistration)
 admin.site.register(StudentRegistration)
+admin.site.register(GeneralRegistration)
--- backend/models.py
+++ backend/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class StudentRegistration(models.Model):
-
+class GeneralRegistration(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     occupation = models.CharField(max_length=250)
     gender = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
+
+
+class StudentRegistration(models.Model):
     email = models.EmailField(max_length=200)
     phone = models.IntegerField()
     country = models.CharField(max_length=100)
@@ -18,15 +23,10 @@ class StudentRegistration(models.Model):
 
 
     def __str__(self):
-        return self.first_name
+        return self.email
 
 
 class EmployerRegistration(models.Model):
-
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    occupation = models.CharField(max_length=250)
-    gender = models.CharField(max_length=250)
     email = models.EmailField(max_length=250)
     phone = models.IntegerField()
     country = models.CharField(max_length=250)
@@ -37,4 +37,4 @@ class EmployerRegistration(models.Model):
     
 
     def __str__(self):
-        return self.business_name
+        return self.business_name + ' ' + self.email
