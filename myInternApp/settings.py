a86da3e8592688f03c24ab81444431b15ae4252e
Adeyemi Adekanye <sundayadeyemi8@gmail.com>
2021-06-30 03:52:35 +0100

    adding aci and serializer

--- myInternApp/settings.py
+++ myInternApp/settings.py
@@ -133,3 +133,9 @@ STATIC_ROOT = str(BASE_DIR / "static")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
